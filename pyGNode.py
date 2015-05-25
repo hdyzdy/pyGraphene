65517802b900f297e8549875fe68cbdc70a7d179
hdyzdy <1057816159@qq.com>
2015-05-25 16:22:58 +0800

    ver.1.1
    good version

--- pyGNode.py
+++ pyGNode.py
@@ -13,6 +13,21 @@ class pyGNode:
         if (len(self.cns) < 3) and (self.ncn != 'solid'):
             bool = True
         return bool
+
+    def setCAHMpos(self,node2,node3):
+        p1 = self.pos
+        p2 = node2.pos
+        p3 = node3.pos
+        v2 = p1 - p2
+        v3 = p1 - p3
+        v4 = v2 + v3
+        v4u = v4/numpy.linalg.norm(v4)
+        p4 = p1 + 1.45*v4u
+        p5 = p4 + 1.3*v4u
+        # print v4,v4u
+        print p4,p5
+        return (p4, p5)
+
     def setR6pos(self,node2,node3,node4):
         #This function sets the positions of a the acetylene atoms in a HACA step
         #by mirroring the existing middle nodes (2,3) across the axis defined by
@@ -42,10 +57,18 @@ class pyGNode:
         return (p4,p5)
 
 if __name__ == '__main__':
-    node1 = pyGNode(pos=numpy.array([-numpy.cos(numpy.pi*2./5),numpy.sin(numpy.pi*2./5),0.]))
-    node2 = pyGNode(pos=numpy.array([0.,0.,0.]))
-    node3 = pyGNode(pos=numpy.array([1.,0.,0.]))
-    (p4,p5) = node1.setR5pos(node2,node3)
+    # node1 = pyGNode(pos=numpy.array([-numpy.cos(numpy.pi*2./5),numpy.sin(numpy.pi*2./5),0.]))
+    # node2 = pyGNode(pos=numpy.array([0.,0.,0.]))
+    # node3 = pyGNode(pos=numpy.array([1.,0.,0.]))
+    # (p4,p5) = node1.setR5pos(node2,node3)
+
+    # print p4, p5
+
+    node1 = pyGNode(pos=numpy.array([0.,0.,0.]))
+    node2 = pyGNode(pos=numpy.array([-1.42*numpy.sin(numpy.pi/6.),-1.42*numpy.cos(numpy.pi/6.),0.]))
+    node3 = pyGNode(pos=numpy.array([1.42*numpy.sin(numpy.pi/6.),-1.42*numpy.cos(numpy.pi/6.),0.]))
+    (p4,p5) = node1.setCAHMpos(node2,node3)
     print p4, p5
+
 
 
